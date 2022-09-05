4f317a02ae15b1fff6a5e372a29ce932985de0ae
Артем Черных <acac1212@yandex.ru>
2022-09-05 12:03:04 +1000

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 const getSquare = (number) => number ** 2;
 
+const squareOfSum = (number) => {
+  const results = [];
+  for (let i = 0; i <= 10; i += 1) {
+    results.push(getSquare(number * 2) + 1);
+  }
+  return results;
+};
+
+export { squareOfSum };
 export default getSquare;
